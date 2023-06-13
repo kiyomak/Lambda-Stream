9be892716ca63e36e8a58bed340eaed5ebc1e5b8
AkinaKiyomatsu <132872992+kiyomak@users.noreply.github.com>
2023-06-13 14:48:19 +0900

    課題提出

--- src/Task.java
+++ src/Task.java
@@ -5,7 +5,6 @@ import java.time.LocalDate;
 
 //Comparableインタフェースを実装する(比較処理→並べ替えに必要)
 public class Task implements Comparable<Task> {
-	//メンバー変数
 	private LocalDate date;
 	private String task;
 	private boolean done; // 追加　完了はtrue, 未完了は false
@@ -24,12 +23,10 @@ public class Task implements Comparable<Task> {
 	public String getTask() {
 		return task;
 	}
-	
 	// boolean型のgetterメソッドはis～とすることもある
 	public boolean isDone() {
 		return done;
 	}
-	
 	// compareToメソッド(大小比較)をオーバーライドする
 	// 日付で並び替えるため、LocalDateでcompareToを行う
 	@Override
@@ -37,7 +34,6 @@ public class Task implements Comparable<Task> {
 		return this.date.compareTo(t.date);  //比較するもの.compareTo(比較されるもの)
 	}
 	
-	//変数出力時に適用されるメソッド
 	@Override
 	public String toString() {
 		return this.date + " " + this.task;
